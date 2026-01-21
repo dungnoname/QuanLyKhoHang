efb900bddaed9998db346d606966a4fcb98963c7
khoapd07 <khoapd07@gmail.com>
2026-01-21 10:00:01 +0700

    Xay dung Backend ban dau

---
+++ Backend/QuanLyKhoHang/src/main/java/com/poly/quanlykhohang/QuanLyKhoHangApplication.java
@@ -0,0 +1,13 @@
+package com.poly.quanlykhohang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyKhoHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyKhoHangApplication.class, args);
+    }
+
+}
---
+++ Backend/QuanLyKhoHang/src/test/java/com/poly/quanlykhohang/QuanLyKhoHangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.quanlykhohang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyKhoHangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
